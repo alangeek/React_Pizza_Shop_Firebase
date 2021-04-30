91ddded8900dd6b6b83e62a27cc81f13330ab688
Alan Christian <alanchristiancreator@hotmail.com>
2021-04-30 19:31:55 -0300

    First Commit Remove Boirplate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        Vamos comer uma pizza hoje
+      </header>
+    </div>
+  );
+}
+
+export default App;
