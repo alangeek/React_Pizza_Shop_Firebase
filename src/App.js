ae3e8e4e0dd005b96796a39c145e3da37db96f43
Alan Christian <alanchristiancreator@hotmail.com>
2021-04-30 19:53:01 -0300

    Add Google Fonts, install styled-components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Open Sans', sans-serif;
+  }
+  
+  h1, h2, h3 {
+    font-family: 'Righteous', cursive;
+  }
+`
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        Vamos comer uma pizza hoje
+       <h1>Vamos comer uma pizza hoje</h1> 
       </header>
     </div>
   );
